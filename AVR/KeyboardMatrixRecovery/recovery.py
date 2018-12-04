83240bc449f55a00c8a957cfd59b7e2e8e1b0238
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2018-12-04 22:46:28 +0100

    AVR/KeyboardMatrixRecovery: add POC.

---
+++ AVR/KeyboardMatrixRecovery/recovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+keys = [
+    "Esc", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12", "Bloc. Scorr", "Pausa", "Ins", "Canc",
+    "\\", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "'", "ì"                  , "Backspace"     , "Home",
+    "TAB", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "è", "+"                 , "Return"        , "Pag. Up",
+    "Caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", "ò", "à", "ù",                                 , "Pag. Down",
+    "Left Shift", "<", "z", "x", "c", "v", "b", "n", "m", ",", ".", "-", "Right Shift",       "Up Arrow", "End",
+    "Left Ctrl", "Fn", "Windows", "Left Alt", "Spacebar", "Right Alt", "Menu", "Right Ctrl", "Left Arrow", "Down Arrow", "Right Arrow",
+]
