d06d8f795a99e466ed9089021b1f1c3a3b53c4cc
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2020-06-07 13:17:00 +0200

    fpga/SOC: add subroutine to the bootrom.

--- fpga/mojo/SOC/modules/blockrams/boot.asm
+++ fpga/mojo/SOC/modules/blockrams/boot.asm
@@ -10,9 +10,9 @@ mul r11, r13
 ld r9, [r0 + 0x24]
 # try to push
 push r9
-nop
-nop
-nop
+ld r1, 0x7
+ld r10, 0x18
+jrrl r10
 # JUMP to halt
 jrrl r8, r12
 nop
@@ -22,3 +22,9 @@ nop
 hl
 # this is loaded above
 .word 0x01020304
+
+# function
+# calling convention: r1, r2, r3, r4 argument, r5 return value
+ld r5, 0x2
+mul r1, r5
+jr r15
