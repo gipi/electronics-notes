4316c65b48ee0d00018b040f629c68226b09cc81
Gianluca Pacchiella <gp@ktln2.org>
2020-07-05 15:03:13 +0200

    fpga/SOC: rework uart tx tests.

--- fpga/mojo/SOC/sim/uart_tx.cpp
+++ fpga/mojo/SOC/sim/uart_tx.cpp
@@ -1,7 +1,56 @@
 #include <stdlib.h>
+#include <plog/Log.h> /* this for all the code that needs the PLOG_* stuff */
+#include <plog/Init.h> /* this only for the initialization */
+#include <plog/Formatters/TxtFormatter.h>
+#include <plog/Appenders/ColorConsoleAppender.h>
+
 #include "Vuart_tx.h"
 #include "verilated_vcd_c.h"
 #include "verilated.h"
+
+uint64_t tickcount = 0;
+
+typedef enum {
+    IDLE,
+    TRANSMITTING
+} tx_state_t;
+
+size_t tx_counter = 0;
+
+tx_state_t tx_state = IDLE;
+unsigned char tx_data;
+
+
+void tickPeripherals(Vuart_tx* v) {
+    // if in reset state we have nothing to do
+    if (!v->reset)
+        return;
+
+    switch (tx_state) {
+        case IDLE:
+            if (!v->o_tx) {
+                PLOG_DEBUG << " [+] tx starting communications";
+                tx_counter = 0;
+                tx_data = 0x00;
+                tx_state = TRANSMITTING;
+            }
+            break;
+
+        case TRANSMITTING:
+            if (tx_counter < 8) {
+                //PLOG_DEBUG << v->o_tx;
+                tx_data = tx_data | v->o_tx << (tx_counter);
+                tx_counter++;
+            } else {
+                std::stringstream ss;
+                ss << "transmitted byte: '" << tx_data << "' (" << std::setfill('0') << std::setw(1) << std::hex << static_cast<int>(tx_data) << ")";
+                PLOG_DEBUG << ss.str();
+                tx_state = IDLE;
+                tx_counter = 0;
+            }
+            break;
+    }
+}
 
 
 void tick(uint64_t tickcount, Vuart_tx* v, VerilatedVcdC* tfp) {
@@ -18,6 +67,26 @@ void tick(uint64_t tickcount, Vuart_tx* v, VerilatedVcdC* tfp) {
         tfp->dump(tickcount*10 + 5);
         tfp->flush();
     }
+
+    tickPeripherals(v);
+}
+
+
+void transmit(Vuart_tx *tx, uint8_t data, VerilatedVcdC* trace) {
+    tx->i_start = 1;
+    tx->i_data = data;
+
+    tick(++tickcount, tx, trace);
+
+    tx->i_start = 0;
+
+    assert(tx->o_done == 0);
+
+    for (unsigned int count = 0; count < 20 ; count++) {
+        tick(++tickcount, tx, trace);
+    }
+
+    assert(tx->o_done == 1);
 }
 
 int main(int argc, char* argv[]) {
@@ -29,10 +98,13 @@ int main(int argc, char* argv[]) {
 
     Vuart_tx* tx = new Vuart_tx;
 
+    static plog::ColorConsoleAppender<plog::TxtFormatter> consoleAppender(plog::streamStdErr);
+    plog::init(plog::verbose, &consoleAppender);
+
+    PLOG_INFO << " [+] start tracing";
     tx->trace(trace, 99);
     trace->open("uart_tx.vcd");
 
-    uint64_t tickcount = 0;
     tx->reset = 0;
 
     // Tick the clock until we are done
@@ -42,17 +114,10 @@ int main(int argc, char* argv[]) {
 
     tx->reset = 1;
 
-    tx->i_start = 1;
-    tx->i_data = 0x55;
-
-    tick(++tickcount, tx, trace);
-
-    tx->i_start = 0;
-
-    for (unsigned int count = 0; count < 15 ; count++) {
-        tick(++tickcount, tx, trace);
-    }
-
+    transmit(tx, 0xaa, trace);
+    transmit(tx, 0x55, trace);
+    transmit(tx, 'h', trace);
+    transmit(tx, 0x01, trace);
 
     return EXIT_SUCCESS;
 }
