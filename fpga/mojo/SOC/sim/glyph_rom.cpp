c845c764973572083d9de22d8c7fc2fa5dfe9735
Gianluca Pacchiella <gp@ktln2.org>
2020-12-16 20:18:59 +0100

    fpga/SOC: improve simulation for glyph ROM.

--- fpga/mojo/SOC/sim/glyph_rom.cpp
+++ fpga/mojo/SOC/sim/glyph_rom.cpp
@@ -1,9 +1,42 @@
 #include <stdlib.h>
 #include "Vglyph_rom.h"
 #include "verilated.h"
+#include "verilated_vcd_c.h"
 
 #define LOG(...) fprintf(stderr, __VA_ARGS__)
 
+
+uint8_t tick(Vglyph_rom* rom, VerilatedVcdC* t, uint64_t tickcount) {
+        rom->i_wb_stb = 0;
+        rom->clk = 0;
+        rom->eval();
+        t->dump(tickcount*50);
+
+        rom->i_wb_stb = 1;
+        rom->clk = 1;
+        rom->eval();
+        t->dump(tickcount*50 + 10);
+
+        uint8_t value = rom->o_data;
+
+        rom->i_addr = 0x22a1;
+        rom->i_wb_stb = 0;
+        rom->clk = 0;
+        rom->eval();
+        t->dump(tickcount*50 + 20);
+
+        rom->i_wb_stb = 0;
+        rom->clk = 1;
+        rom->eval();
+        t->dump(tickcount*50 + 30);
+
+        rom->i_wb_stb = 0;
+        rom->clk = 0;
+        rom->eval();
+        t->dump(tickcount*50 + 40);
+
+    return value;
+}
 
 int main(int argc, char *argv[]) {
     LOG(" [+] starting Glyph ROM simulation\n");
@@ -11,15 +44,16 @@ int main(int argc, char *argv[]) {
 
     Vglyph_rom* g_rom = new Vglyph_rom;
 
+    Verilated::traceEverOn(true);
+    VerilatedVcdC* trace = new VerilatedVcdC;
+
+    g_rom->trace(trace, 99);
+    trace->open("glyph_rom.vcd");
+
     printf("[character code %x]\n", tickcount >> 7);
     for ( ; tickcount < 16834 ; ) {
         g_rom->i_addr = tickcount;
-        // printf(" [character code %x]", tickcount);
-        g_rom->clk = 0;
-        g_rom->eval();
-
-        g_rom->clk = 1;
-        g_rom->eval();
+        uint8_t value = tick(g_rom, trace, tickcount);
 
         if (((tickcount % 8) == 0) && tickcount) {
             printf("\n");
@@ -28,11 +62,7 @@ int main(int argc, char *argv[]) {
         if ((tickcount % (8 * 16)) == 0 && tickcount) {
             printf("[character code %x]\n", tickcount >> 7);
         }
-        printf("%c", g_rom->o_data ? '#' : ' ');
-
-
-        g_rom->clk = 0;
-        g_rom->eval();
+        printf("%c", value ? '#' : ' ');
 
         tickcount++;
     }
